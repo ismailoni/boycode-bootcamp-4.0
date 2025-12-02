da3296540a1b8e755e22bc75a6d7e1d8b261e8a0
Oni Ismail Modupe <ismailoni80@gmail.com>
2025-12-02 02:41:54 +0100

    Feat: add bonus features for top rated movies and movie stats

--- assessment-week-6/Team A - movie-review Api/controllers/movieController.js
+++ assessment-week-6/Team A - movie-review Api/controllers/movieController.js
@@ -73,3 +73,23 @@ exports.partialUpdateMovie = (req, res) => {
     const updatedMovie = { ...results[movieIndex], ...req.body };
     res.status(200).json(updatedMovie);
 }
+
+// Bonus Feature: Get top rated movies
+exports.getTopRatedMovies = (req, res) => {
+    const topRatedMovies = results.filter(m => m.rating >= 4.5);
+    res.status(200).json({ total: topRatedMovies.length, results: topRatedMovies });
+}
+
+// Bonus Feature: Get movie stats(number of reviews, average rating)
+exports.getMovieStats = (req, res) => {
+    const movieId = parseInt(req.params.id);
+    const movie = results.find(m => m.id === movieId);
+    if (!movie) {
+        return res.status(404).json({ message: 'Movie not found' });
+    }
+    const movieReviews = reviews.filter(r => r.movieId === movieId);
+    const numberOfReviews = movieReviews.length;
+    const averageRating = movieReviews.reduce((acc, curr) => acc + curr.rating, 0) / numberOfReviews || 0;
+    res.status(200).json({ movieId, numberOfReviews, averageRating: averageRating.toFixed(2) });
+}
+
